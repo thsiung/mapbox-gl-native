a5ff7fcf0c02af63d03c5af6f64dd3ad207df733
tobrun <tobrun.van.nuland@gmail.com>
2018-09-14 17:26:49 +0200

    [android] - avoid failing CI tests when device under test doesn't have a valid internet connection

--- platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/activity/BaseActivityTest.java
+++ platform/android/MapboxGLAndroidSDKTestApp/src/androidTest/java/com/mapbox/mapboxsdk/testapp/activity/BaseActivityTest.java
@@ -1,27 +1,21 @@
 package com.mapbox.mapboxsdk.testapp.activity;
 
 import android.app.Activity;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.support.test.espresso.IdlingRegistry;
 import android.support.test.espresso.IdlingResourceTimeoutException;
 import android.support.test.espresso.ViewInteraction;
 import android.support.test.rule.ActivityTestRule;
-
+import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.testapp.R;
 import com.mapbox.mapboxsdk.testapp.action.MapboxMapAction;
 import com.mapbox.mapboxsdk.testapp.action.WaitAction;
 import com.mapbox.mapboxsdk.testapp.utils.OnMapReadyIdlingResource;
-
 import junit.framework.Assert;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.rules.TestName;
-
 import timber.log.Timber;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -60,7 +54,10 @@ public abstract class BaseActivityTest {
   }
 
   protected void validateTestSetup() {
-    Assert.assertTrue("Device is not connected to the Internet.", isConnected(rule.getActivity()));
+    if (!Mapbox.isConnected()) {
+     Timber.e("Not connected to the internet while running test");
+    }
+
     checkViewIsDisplayed(R.id.mapView);
     Assert.assertNotNull(mapboxMap);
   }
@@ -81,13 +78,6 @@ public abstract class BaseActivityTest {
 
   protected void waitAction(long waitTime) {
     onView(withId(R.id.mapView)).perform(new WaitAction(waitTime));
-  }
-
-  static boolean isConnected(Context context) {
-    ConnectivityManager connectivityManager
-      = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-    NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-    return activeNetworkInfo != null && activeNetworkInfo.isConnected();
   }
 
   protected ViewInteraction onMapView() {
